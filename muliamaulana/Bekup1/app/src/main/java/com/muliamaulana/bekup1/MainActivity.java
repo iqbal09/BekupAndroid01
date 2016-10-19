828ce90027da2fe6b664fad4c74f987442c5da12
muliamaulana <halo@muliamaulana.com>
2016-10-20 00:09:42 +0700

    commitmulia

--- muliamaulana/Bekup1/app/src/main/java/com/muliamaulana/bekup1/MainActivity.java
+++ muliamaulana/Bekup1/app/src/main/java/com/muliamaulana/bekup1/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("REGISTER");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Spinner spinner = (Spinner) findViewById(R.id.mobile_option);
+        ArrayAdapter<CharSequence> spinner_adapter = ArrayAdapter.createFromResource(this,R.array.mobile, R.layout.support_simple_spinner_dropdown_item);
+        spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinner_adapter);
     }
 }
